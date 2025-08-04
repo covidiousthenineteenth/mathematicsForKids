7d49b876b02decde04de168c4769f4e9aad36dbe
covidiousthenineteenth <pomajuniors@gmail.com>
2025-08-04 16:00:38 -0700

    fixing footer center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ function Footer()
 {
 
     return (<footer className="navbar fixed-bottom">
-    <div className="container-fluid">
+    <div className="container-fluid d-flex justify-content-center">
      <a href="https://portfolio-website-juanpoma.vercel.app" className="nav-link text-light">
          Copyright &copy; {new Date().getFullYear()} Juan Poma
     </a>
      </div>
-</footer>)
+</footer>);
 }
 export default Footer
